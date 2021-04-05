a05621dd372430cb47a06bb11650551167e88242
Mateus Fonseca <mateusrfon@gmail.com>
2021-04-05 15:04:48 -0300

    inicio imp msg wpp

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ let desert
 
 function SelectDish(buttonClass) {
     const dishes = document.querySelector('.dishes .selected')
-    if (dish != null) {
+    if (dishes != null) {
         dishes.classList.remove('selected')
     }
 
     const select = document.querySelector(buttonClass + ' .selected-border')
     select.classList.add('selected')
 
-    dish = true;
-    done()
+    dish = buttonClass;
+    Done()
 }
 
 function SelectDrink(buttonClass) {
@@ -24,8 +24,8 @@ function SelectDrink(buttonClass) {
     const select = document.querySelector(buttonClass + ' .selected-border')
     select.classList.add('selected')
 
-    drink = true;
-    done()
+    drink = buttonClass;
+    Done()
 }
 
 function SelectDesert(buttonClass) {
@@ -37,14 +37,44 @@ function SelectDesert(buttonClass) {
     const select = document.querySelector(buttonClass + ' .selected-border')
     select.classList.add('selected')
 
-    desert = true;
-    done()
+    desert = buttonClass;
+    Done()
 }
 
-function done(){
-    if (dish === true && drink === true && desert == true) {
+function Done(){
+    if (dish != null && drink != null && desert != null) {
         const done = document.querySelector('.end button');
         done.classList.add('bgreen')
         done.querySelector('p').innerHTML = 'Fechar pedido'
+    }
+}
+
+function Close() {
+    if (dish != null && drink != null && desert != null) {
+    const dishes = document.querySelector('.dishes ' + dish);
+    const drinks = document.querySelector('.drinks ' + drink);
+    const deserts = document.querySelector('.deserts ' + desert);
+
+    const pdish = dishes.querySelector('.price').innerHTML - "R$ ";
+    const pdrink = drinks.querySelector('.price').innerHTML - "R$ ";
+    const pdesert = deserts.querySelector('.price').innerHTML - "R$ ";
+
+    const total = parseInt(pdish) + parseInt(pdrink) + parseInt(pdesert);
+
+    let msg = "Olá, gostaria de fazer o pedido:" +
+    "- Prato: " + dishes.querySelector('.goods strong').innerHTML +
+    "\n- Bebida: " + drinks.querySelector('.goods strong').innerHTML +
+    "\n- Sobremesa: " + deserts.querySelector('.goods strong').innerHTML +
+    "\nTotal: R$ " + total;
+
+    msg = window.encodeURIComponent(msg);
+    SendMsg(msg);
+
+    }
+
+    function SendMsg(mensagem) {
+        const celular = "5571992862087";
+        const msg = mensagem;
+        window.open("https://wa.me/" + celular + "?text=" + msg, "_blank")
     }
 }
